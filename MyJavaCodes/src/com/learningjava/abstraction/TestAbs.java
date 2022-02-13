60faf174cbe70f84bd454654d6925261bbd68ba6
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2022-02-13 22:05:20 +0530

    Addedall the codes together

--- MyJavaCodes/src/com/learningjava/abstraction/TestAbs.java
+++ MyJavaCodes/src/com/learningjava/abstraction/TestAbs.java
@@ -6,8 +6,11 @@ public class TestAbs {
 		Circle c1 = new Circle(3, "Green");
 		Rectangle r1 = new Rectangle(10, 8, "Blue");
 		
+		Shape obj = new Rectangle(5, 10, "red");
+		
 		System.out.println(c1);
 		System.out.println(r1);
+		System.out.println(obj);
 
 	}
 
